af8cd79ebbe77091d5376be6fac20dbd0628e15d
AshameTheDestroyer <hashemwnoos@gmail.com>
2026-02-04 18:10:13 +0300

    Colors Adjusted.

---
+++ frontend/app/components/sidebar-provider.tsx
@@ -0,0 +1,57 @@
+import { Avatar } from "radix-ui";
+import { Link, useLocation } from "react-router";
+import { Building2Icon, LayoutDashboardIcon } from "lucide-react";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarInset, SidebarProvider as SidebarProvider_ } from "./ui/sidebar";
+
+const LINKS = [
+    { title: "Jobs", to: "/", icon: Building2Icon },
+    { title: "Dashboard", to: "/dashboard", icon: LayoutDashboardIcon },
+]
+
+export function SidebarProvider({ children }: { children: React.ReactNode }) {
+    const { pathname } = useLocation();
+    return (
+        <SidebarProvider_>
+            <Sidebar collapsible="icon" className="bg-indigo-100 dark:bg-slate-950">
+                <SidebarContent className="p-4">
+                    {LINKS.map(({title,to,icon: Icon}, i) => (
+                        <Link
+                            key={i}
+                            to={to}
+                            title={title}
+                            className={`flex place-items-center group-data-[collapsible=icon]:place-content-center gap-2 px-4 py-2 group-data-[collapsible=icon]:p-0 group-data-[collapsible=icon]:size-8 group-data-[collapsible=icon]:-mx-2 hover:bg-indigo-200 dark:hover:bg-slate-800 rounded-md ${pathname === to ? "bg-indigo-300 dark:bg-slate-700" : ""}`}
+                        >
+                            <Icon />{" "}
+                            <span className="group-data-[collapsible=icon]:hidden">{title}</span>
+                        </Link>
+                    ))}
+                </SidebarContent>
+                <SidebarFooter className="p-4">
+                    <div className="group-data-[collapsible=icon]:-mx-2 flex gap-2">
+                        <Avatar.Avatar className="shrink-0">
+                            <Avatar.AvatarImage
+                                src="https://github.com/ashamethedestroyer.png"
+                                alt="@ashamethedestroyer"
+                                className="size-10 rounded-full group-data-[collapsible=icon]:size-8"
+                            />
+                            <Avatar.AvatarFallback className="rounded-full bg-gray-200 dark:bg-gray-800 size-10 group-data-[collapsible=icon]:size-8 flex items-center justify-center">
+                                A
+                            </Avatar.AvatarFallback>
+                        </Avatar.Avatar>
+                        <div className="overflow-hidden group-data-[collapsible=icon]:hidden">
+                            <p className="font-bold whitespace-nowrap text-ellipsis overflow-hidden">
+                                Hashem Wannous
+                            </p>
+                            <p className="whitespace-nowrap text-ellipsis overflow-hidden">
+                                @ashamethedestroyer
+                            </p>
+                        </div>
+                    </div>
+                </SidebarFooter>
+            </Sidebar>
+            <SidebarInset className="flex flex-col h-screen">
+                {children}
+            </SidebarInset>
+        </SidebarProvider_>
+    );
+}
