059331c52d1ae39eb324db7d067051895c76e9e5
AshameTheDestroyer <hashemwnoos@gmail.com>
2026-02-04 18:10:13 +0300

    Colors Adjusted.

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,5 +1,4 @@
 import {
-    Link,
     Meta,
     Links,
     Outlet,
@@ -7,11 +6,9 @@ import {
     ScrollRestoration,
     isRouteErrorResponse,
 } from "react-router";
-import { Avatar } from "radix-ui";
 import type { Route } from "./+types/root";
 import { Header } from "./components/header";
-import { Building2Icon, LayoutDashboardIcon } from "lucide-react";
-import { Sidebar, SidebarContent, SidebarFooter, SidebarInset, SidebarProvider } from "./components/ui/sidebar";
+import { SidebarProvider } from "./components/sidebar-provider";
 
 import "./app.css";
 
@@ -30,36 +27,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
             </head>
             <body>
                 <SidebarProvider>
-                    <Sidebar collapsible="icon">
-                        <SidebarContent className="p-4">
-                            <Link to="/" title="Jobs" className="flex place-items-center group-data-[collapsible=icon]:place-content-center gap-2 px-4 py-2 group-data-[collapsible=icon]:p-0 group-data-[collapsible=icon]:size-8 group-data-[collapsible=icon]:-mx-2 hover:bg-gray-200 dark:hover:bg-gray-800 rounded-md">
-                                <Building2Icon /> <span className="group-data-[collapsible=icon]:hidden">Jobs</span>
-                            </Link>
-                            <Link to="/dashboard" title="Dashboard" className="flex place-items-center group-data-[collapsible=icon]:place-content-center gap-2 px-4 py-2 group-data-[collapsible=icon]:p-0 group-data-[collapsible=icon]:size-8 group-data-[collapsible=icon]:-mx-2 hover:bg-gray-200 dark:hover:bg-gray-800 rounded-md">
-                                <LayoutDashboardIcon /> <span className="group-data-[collapsible=icon]:hidden">Dashboard</span>
-                            </Link>
-                        </SidebarContent>
-                        <SidebarFooter className="p-4">
-                            <div className="group-data-[collapsible=icon]:-mx-2 flex gap-2">
-                               <Avatar.Avatar className="shrink-0">
-                                    <Avatar.AvatarImage
-                                        src="https://github.com/ashamethedestroyer.png"
-                                        alt="@ashamethedestroyer"
-                                        className="size-10 rounded-full group-data-[collapsible=icon]:size-8"
-                                    />
-                                    <Avatar.AvatarFallback className="rounded-full bg-gray-200 dark:bg-gray-800 size-10 group-data-[collapsible=icon]:size-8 flex items-center justify-center">A</Avatar.AvatarFallback>
-                                </Avatar.Avatar>
-                                <div className="overflow-hidden group-data-[collapsible=icon]:hidden">
-                                    <p className="font-bold whitespace-nowrap text-ellipsis overflow-hidden">Hashem Wannous</p>
-                                    <p className="whitespace-nowrap text-ellipsis overflow-hidden">@ashamethedestroyer</p>
-                                </div>
-                            </div>
-                        </SidebarFooter>
-                    </Sidebar>
-                    <SidebarInset className="flex flex-col h-screen">
-                        <Header />
-                        <main>{children}</main>
-                    </SidebarInset>
+                    <Header />
+                    <main>{children}</main>
                 </SidebarProvider>
                 <ScrollRestoration />
                 <Scripts />
