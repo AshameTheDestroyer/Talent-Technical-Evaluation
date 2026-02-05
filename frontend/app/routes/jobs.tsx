c2c23d92c40590bad6ee0bae8581b3e89488799d
AshameTheDestroyer <hashemwnoos@gmail.com>
2026-02-05 17:53:05 +0300

    Create New Job Page.

--- frontend/app/routes/jobs.tsx
+++ frontend/app/routes/jobs.tsx
@@ -1,4 +1,3 @@
-import { Loader2Icon } from "lucide-react";
 import type { Route } from "./+types/jobs";
 import { useNavigate } from "react-router";
 import { JobCard } from "~/components/job-card";
