d3dd51d5e96b675c2827e7f72b595ad6c3cddf04
AshameTheDestroyer <hashemwnoos@gmail.com>
2026-02-05 17:02:49 +0300

    Colors Adjusted.

--- frontend/app/routes/jobs.$jid.assessment.$aid.applications.tsx
+++ frontend/app/routes/jobs.$jid.assessment.$aid.applications.tsx
@@ -66,10 +66,10 @@ export default function AssessmentDetailRoute() {
                     {applications.length === 0 ? (
                         <p>No applications found for this assessment.</p>
                     ) : applications.map(application => (
-                        <div key={application.id} className="border p-4 rounded bg-indigo-50 dark:bg-gray-700 flex flex-wrap justify-evenly gap-4 place-items-center">
+                        <div key={application.id} className="border p-4 rounded bg-indigo-100 dark:bg-gray-700 flex flex-wrap justify-evenly gap-4 place-items-center">
                             <div className="group-data-[collapsible=icon]:-mx-4 flex gap-2">
                                 <Avatar.Avatar className="shrink-0 cursor-pointer" tabIndex={0}>
-                                    <Avatar.AvatarFallback className="rounded-full bg-gray-200 dark:bg-gray-800 size-10 group-data-[collapsible=icon]:size-8 flex items-center justify-center">
+                                    <Avatar.AvatarFallback className="rounded-full bg-indigo-200 dark:bg-gray-800 size-10 group-data-[collapsible=icon]:size-8 flex items-center justify-center">
                                         {application.user ? `${application.user.first_name[0]}${application.user.last_name[0]}` : "U"}
                                     </Avatar.AvatarFallback>
                                 </Avatar.Avatar>
